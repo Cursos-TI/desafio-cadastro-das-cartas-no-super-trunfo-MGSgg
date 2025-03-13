411b1904f28d15abe2db69287df6e8ae32de40a2
MGSgg <71718205+MGSgg@users.noreply.github.com>
2025-03-13 02:16:01 +0000

    terminei o nivel novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,58 +1,59 @@
 #include <stdio.h>
 
-   int main(){
-
+ int main (){
+ 
        char estado;
-       char codigo;
+       char codigo[3];
        char cidade[10];
        int  populacao;
        float area;
        float PIB;
-       int PT;
-       float DP;
-       float PPC;
-
-    
-       printf("\nEntre com a inicial do estado: \n");
-       scanf(" %c", &estado);
+       int PontoTour;
        
-       printf("\nEntre com um número de 1 a 4: \n");
-       scanf(" %c", &codigo);
+    
+       printf("\nEntre com a inicial do estado: \n"); // Imprime ao usuário a solicitação de inserir a inicial do estado
+       scanf(" %c", &estado); // Lê os dados inseridos pelo usuário e os atribui a variável estado
+       
+       printf("\nEntre com um número de 01 a 04: \n"); // Imprime ao usuário a solicitação de inserir um número de 1 a 4
+       scanf(" %s", codigo); // Lê os dados inseridos pelo usuário e os atribui a variável codigo
 
-       printf("\nEntre com a cidade: \n");
-       scanf(" %s", cidade);
+       printf("\nEntre com a cidade: \n"); // Imprime ao usuário a solicitação de inserir a cidade
+       scanf(" %s", cidade);// Lê os dados inseridos pelo usuário e os atribui a variável cidade
 
-       printf("\nEntre com a populacao:\n");
-       scanf("\n %i", &populacao);
+       printf("\nEntre com a população:\n"); // Imprime ao usuário a solicitação de inserir a população 
+       scanf("\n %i", &populacao); // // Lê os dados inseridos pelo usuário e os atribui a variável população
 
-       printf("\nEntre com a area: \n");
-       scanf(" %f", &area);
+       printf("\nEntre com a área: \n"); // Imprime ao usuário a solicitação de inserir a área
+       scanf(" %f", &area); // Lê os dados inseridos pelo usuário e os atribui a variável área
 
-       printf("\nEntre com o PIB: \n");
-       scanf(" %f", &PIB);
-       printf("\nEntre com os Pontos Turísticos: \n");
-       scanf(" %i", &PT);
+       printf("\nEntre com o PIB: \n"); // // Imprime ao usuário a solicitação de inserir o PIB
+       scanf(" %f", &PIB); // Lê os dados inseridos pelo usuário e os atribui a variável PIB
 
-        DP = populacao / area;
-        PPC = PIB / populacao;
+       printf("\nEntre com os Pontos Turísticos: \n"); // Imprime ao usuário a solicitação de inserir a quantidade de pontos turísticos
+       scanf(" %i", &PontoTour); // Lê os dados inseridos pelo usuário e os atribui a variável Pontos Turísticos
 
+       printf("\n        Informações da Carta 1 %c", estado);
+       printf("%s \n", codigo);
+       printf("\nCidade:  %s \n", cidade);
+       printf("\nPopulação: %i habitantes \n", populacao);
+       printf("\nÁrea:  %f km² \n ", area);
+       printf("\nPIB: R$ %f \n ", PIB);
+       printf("\nPontos Turísticos:  %i \n ", PontoTour);
     
 
        char estado1;
-       char codigo1;
+       char codigo1[3];
        char cidade1[10];
        int  populacao1;
        float area1;
        float PIB1;
-       int PT1;
-       float DP1;
-       float PPC1;
+       int PontoTour1;
 
        printf("\nEntre com a inicial do estado 2 : \n");
        scanf(" %c", &estado1);
        
-       printf("\nEntre com um número de 1 a 4: \n");
-       scanf(" %c", &codigo1);
+       printf("\nEntre com um número de 01 a 04: \n");
+       scanf(" %s", &codigo1);
 
        printf("\nEntre com o estado 2 : \n");
        scanf(" %s", cidade1);
@@ -66,32 +67,23 @@
        printf("\nEntre com o PIB do estado 2: \n");
        scanf(" %f", &PIB1);
        printf("\nEntre com os Pontos Turísticos do estado 2: \n");
-       scanf(" %i", &PT1);
+       scanf(" %i", &PontoTour1);
 
 
-       DP1 = populacao1 / area1;
-       PPC1 = PIB1 / populacao1;
-
-     
-   
        printf("\n        Informações da Carta 1 %c", estado);
-       printf("0%c \n", codigo);
+       printf("%s \n", codigo);
        printf("\nCidade:  %s \n", cidade);
        printf("\nPopulação: %i habitantes \n", populacao);
        printf("\nÁrea:  %f km² \n ", area);
        printf("\nPIB: R$ %f \n ", PIB);
-       printf("\nPontos Turísticos:  %i \n ", PT);
-       printf("\nDensidade Populacional:  %f \n", DP);
-       printf("\nPIB per capita:  %f \n", PPC);
+       printf("\nPontos Turísticos:  %i \n ", PontoTour);
+
 
        printf("\n        Informações da Carta 2 %c", estado1);
-       printf("0%c \n", codigo1);
+       printf("%s \n", codigo1);
        printf("\nCidade:  %s \n", cidade1);
        printf("\nPopulação:  %i habitantes \n", populacao1);
        printf("\nÁrea:  %f km² \n ", area1);
        printf("\nPIB: R$ %f \n ", PIB1);
-       printf("\nPontos Turísticos:  %i \n ", PT1);
-       printf("\nDensidade Populacional:  %f \n", DP1);
-       printf("\nPIB per capita:  %f \n", PPC1);
-       
-       }
+       printf("\nPontos Turísticos:  %i \n ", PontoTour1);
+}
